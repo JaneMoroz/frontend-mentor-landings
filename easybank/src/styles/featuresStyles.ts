f923dcfaf31565735c41c6fc2acc07e07a459652
JaneMoroz <moroz.jane@yahoo.com>
2022-12-07 16:18:42 +0300

    feat: add responsive features

--- easybank/src/styles/featuresStyles.ts
+++ easybank/src/styles/featuresStyles.ts
@@ -3,6 +3,10 @@ import styled, { css } from "styled-components";
 export const FeaturesContainer = styled.div`
   padding: 6.4rem 0;
   background: ${(props) => props.theme.lightGrayishBlue};
+
+  @media only screen and (max-width: 56.25em) {
+    text-align: center;
+  }
 `;
 
 export const FeaturesList = styled.ul`
@@ -10,10 +14,19 @@ export const FeaturesList = styled.ul`
   align-items: flex-start;
   column-gap: 1.6rem;
   padding: 3.2rem 0;
+
+  @media only screen and (max-width: 56.25em) {
+    flex-direction: column;
+    row-gap: 3.2rem;
+  }
 `;
 
 export const FeatureListItem = styled.li`
   display: flex;
   flex-direction: column;
   row-gap: 2.4rem;
+
+  @media only screen and (max-width: 56.25em) {
+    align-items: center;
+  }
 `;
