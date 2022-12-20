184cf3de3e1aae4bd22895f5c0f72b4eb4f6ae6d
JaneMoroz <moroz.jane@yahoo.com>
2022-12-20 15:17:10 +0300

    feat: add features

---
+++ shortly/src/components/Features/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+// Styles
+import "./style.css";
+
+// Icons
+import {
+  BrandIcon,
+  RecordsIcon,
+  CustomizableIcon,
+} from "../../assets/icons/icons";
+
+// Features
+const features = [
+  {
+    icon: <BrandIcon />,
+    title: "Brand Recognition",
+    text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+  },
+  {
+    icon: <RecordsIcon />,
+    title: "Detailed Records",
+    text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+  },
+  {
+    icon: <CustomizableIcon />,
+    title: "Fully Customizable",
+    text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+  },
+];
+
+const Features = () => {
+  return (
+    <section className="features">
+      <div className="container">
+        <div className="title">
+          <h2>Advanced Statistics</h2>
+          <p className="paragraph paragraph--medium">
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+        </div>
+        <ul className="features-list">
+          {features.map((feature, index) => (
+            <li key={index} className="feature-list__item">
+              <div>{feature.icon}</div>
+              <h3>{feature.title}</h3>
+              <p className="paragraph paragraph--small">{feature.text}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- shortly/src/components/index.tsx
+++ shortly/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Hero from "./Hero";
 import Form from "./Form";
+import Features from "./Features";
 
-export { Header, Hero, Form };
+export { Header, Hero, Form, Features };
