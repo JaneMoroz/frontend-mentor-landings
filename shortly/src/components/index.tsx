9192fb005c5f81b3d37d16b995912269eeaf0163
JaneMoroz <moroz.jane@yahoo.com>
2022-12-20 16:06:39 +0300

    feat: add footer

--- shortly/src/components/index.tsx
+++ shortly/src/components/index.tsx
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import Form from "./Form";
 import Features from "./Features";
 import CTA from "./CTA";
+import Footer from "./Footer";
 
-export { Header, Hero, Form, Features, CTA };
+export { Header, Hero, Form, Features, CTA, Footer };
