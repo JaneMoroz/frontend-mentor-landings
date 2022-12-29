e17dd24fbae4da446f53a272e91f528442202010
JaneMoroz <moroz.jane@yahoo.com>
2022-12-29 22:12:16 +0300

    fix: fix summary style bug

--- multi-step-form/src/styles/sharedLayoutStyles.ts
+++ multi-step-form/src/styles/sharedLayoutStyles.ts
@@ -6,12 +6,12 @@ export const StepContainer = styled.div`
 
 export const StepDetails = styled.section`
   padding: 5.6rem 10rem 3.2rem 10rem;
-  min-width: 65rem;
+  width: 65rem;
   min-height: 60rem;
 
   @media only screen and (max-width: 56.25em) {
     padding: 3.2rem 2.4rem;
-    min-width: 100%;
+    width: 100%;
     min-height: 100%;
   }
 `;
